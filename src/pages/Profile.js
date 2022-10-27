03efcca92bb846eab63099edaf25b600b2479af7
pedroreisrimoldi <pedroreisrimoldi@gmail.com>
2022-10-27 17:37:52 -0300

    req 58 e 59

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const [profile, setProfile] = useState('');
+
+  const loginEmail = () => {
+    const user = window.localStorage.getItem('user');
+    if (user === null) {
+      setProfile('');
+    } else {
+      const { email } = JSON.parse(user);
+      setProfile(email);
+    }
+  };
+  useEffect(() => loginEmail(), []);
+
+  console.log(JSON.parse(window.localStorage.getItem('user')));
   return (
     <div>
       <Header
         title="Profile"
       />
-      <h3 data-testid="profile-email"> email </h3>
+      <h3 data-testid="profile-email">{ profile }</h3>
       <button
         data-testid="profile-done-btn"
         type="button"
